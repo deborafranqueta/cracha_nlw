030a97fb168e7f709fba55149bd7ae3c3ac4ccb0
Débora Franquêta <debora_franqueta@hotmail.com>
2021-10-22 20:03:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function getGithubProfileInfos(){
+  const url = "https://api.github.com/users/deborafranqueta"
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+  })
+}
+
+getGithubProfileInfos()
